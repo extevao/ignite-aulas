d81e471ab0dc2e9130abbbbf870a6bbce331a73e
Estevao <estevaovasques@hotmail.com>
2021-07-19 20:37:23 -0300

    criando funcionamento dos botoes de entrada e saida

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 
 import Modal from 'react-modal'
 import closeImgm from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 
 Modal.setAppElement('#root');
@@ -30,8 +32,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
         <input type="number" placeholder="Valor" />
 
-        <input type="text" placeholder="Categoria" />
+        <TransactionTypeContainer>
 
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saída</span>
+          </button>
+
+        </TransactionTypeContainer>
+
+        <input type="text" placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
       </Container>
