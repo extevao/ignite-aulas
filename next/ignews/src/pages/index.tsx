e5376fd9b66f7986dcf916b0a38f78c1e537ec62
Estevao <estevaovasques@hotmail.com>
2021-07-29 21:26:55 -0300

    consumindo api do stripe com ssr

--- next/ignews/src/pages/index.tsx
+++ next/ignews/src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
 import styles from './home.module.scss'
 
-import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,14 +27,32 @@ export default function Home() {
           <h1> News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JIhntHYmqdYcRMtJ5RH9sqN')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-Us', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100), // convertendo para "reais" pq o unit_amount vem em centavos
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
