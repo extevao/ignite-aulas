224bbebe63563831cdc0721e9f2fb637b562aa06
Estevao <estevaovasques@hotmail.com>
2021-08-13 07:59:29 -0300

    Paginacao e form para novo usuario

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 
@@ -81,8 +82,9 @@ export default function UserList() {
                 </Th>
               </Tr>
             </Tbody>
-
           </Table>
+
+          <Pagination />
         </Box>
 
       </Flex>
