77167a7ea4a4c90089000e2d80d9b9de902afa8b
Estevao <estevaovasques@hotmail.com>
2021-08-14 12:26:37 -0300

    responsividade

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -11,15 +11,20 @@ import {
   Text,
   Th,
   Thead,
-  Tr
+  Tr,
+  useBreakpointValue
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -48,17 +53,17 @@ export default function UserList() {
           >
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th> Usuário </Th>
-                <Th> Data de cadastro </Th>
+                {isWideVersion && (<Th> Data de cadastro </Th>)}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -67,7 +72,7 @@ export default function UserList() {
                     <Text fontSize="small" color="gray.300">estevao@email.com</Text>
                   </Box>
                 </Td>
-                <Td> 13 de Agosto, 2021</Td>
+                {isWideVersion && (<Td> 13 de Agosto, 2021</Td>)}
                 <Th>
                   <Button
                     as="a"
