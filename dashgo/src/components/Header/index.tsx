ade9d3de07c6cac076f75a4e3470cd5f8f1f7dca
Estevao <estevaovasques@hotmail.com>
2021-08-14 12:11:30 -0300

    sidebar responsiva e menu drawer

--- dashgo/src/components/Header/index.tsx
+++ dashgo/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
@@ -11,6 +13,8 @@ export function Header() {
     lg: true
   })
 
+  const { onOpen } = useSidebarDrawer()
+
   return (
     <Flex
       as="header"
@@ -21,6 +25,17 @@ export function Header() {
       mt="4"
       px="6"
       align="center">
+
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
 
       <Logo />
 
