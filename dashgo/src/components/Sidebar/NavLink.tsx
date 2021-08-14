87652e481db71ef12df9659dae0fba39ae726872
Estevao <estevaovasques@hotmail.com>
2021-08-14 15:38:47 -0300

    Navegao e link ativo

--- dashgo/src/components/Sidebar/NavLink.tsx
+++ dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,19 @@
 import { ElementType } from "react";
-import { Box, Icon, Link, Stack, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
-import { RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-
+import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink";
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
-  children: string
+  children: string;
+  href: string;
 }
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
   )
 }
--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Tr,
   useBreakpointValue
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -36,16 +37,18 @@ export default function UserList() {
           <Flex mb="8" justifyContent="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              cursor="pointer"
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                cursor="pointer"
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table
