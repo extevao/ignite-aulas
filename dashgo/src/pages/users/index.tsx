f26a6ba1c21b6e50f034171d39a98becdd824f4d
Estevao <estevaovasques@hotmail.com>
2021-08-19 08:28:59 -0300

    listagem dos usuários e configurando devtools do react-query

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -28,7 +28,22 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
+  }, {
+    staleTime: 1000 * 5 // 5 segundos
   })
 
 
@@ -91,30 +106,34 @@ export default function UserList() {
                         </Tr>
                       </Thead>
                       <Tbody>
-                        <Tr>
-                          <Td px={['4', '4', '6']}>
-                            <Checkbox colorScheme="pink" />
-                          </Td>
-                          <Td>
-                            <Box>
-                              <Text fontWeight="bold">Estevao B L Vasques </Text>
-                              <Text fontSize="small" color="gray.300">estevao@email.com</Text>
-                            </Box>
-                          </Td>
-                          {isWideVersion && (<Td> 13 de Agosto, 2021</Td>)}
-                          <Th>
-                            <Button
-                              as="a"
-                              size="sm"
-                              fontSize="sm"
-                              colorScheme="purple"
-                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                              cursor="pointer"
-                            >
-                              Editar
-                            </Button>
-                          </Th>
-                        </Tr>
+                        {data.map(user => {
+                          return (
+                            <Tr key={user.id}>
+                              <Td px={['4', '4', '6']}>
+                                <Checkbox colorScheme="pink" />
+                              </Td>
+                              <Td>
+                                <Box>
+                                  <Text fontWeight="bold">{user.name}</Text>
+                                  <Text fontSize="small" color="gray.300">{user.email}</Text>
+                                </Box>
+                              </Td>
+                              {isWideVersion && (<Td>{user.createdAt}</Td>)}
+                              <Th>
+                                <Button
+                                  as="a"
+                                  size="sm"
+                                  fontSize="sm"
+                                  colorScheme="purple"
+                                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                  cursor="pointer"
+                                >
+                                  Editar
+                                </Button>
+                              </Th>
+                            </Tr>
+                          )
+                        })}
                       </Tbody>
                     </Table >
 
