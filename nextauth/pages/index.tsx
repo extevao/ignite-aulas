164cdfde9988fe36de7ac57260adc755d9606bf6
Estevao <estevaovasques@hotmail.com>
2021-08-23 08:53:17 -0300

    Contexto de autenticação

--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -1,8 +1,29 @@
+import { FormEvent, useContext, useState } from "react"
+import { AuthContext } from "../context/AuthContext"
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
-    <div>
-      <h1>Olá</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+
+      <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+
+      <button type="submit"> Entrar </button>
+    </form>
   )
 }
