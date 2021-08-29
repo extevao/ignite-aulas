83477a10cbc682ff227544748455b0ce58e2e896
Estevao <estevaovasques@hotmail.com>
2021-08-29 12:24:45 -0300

    broadcast de logout

--- nextauth/context/AuthContext.tsx
+++ nextauth/context/AuthContext.tsx
@@ -18,6 +18,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(crendentials: SignInCredentials): Promise<void>;
+  signOut: () => void;
   user: User;
   isAuthenticated: boolean;
 }
@@ -29,9 +30,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+let authChannel: BroadcastChannel
+
 export function signOut() {
   destroyCookie(undefined, 'nextauth.token')
   destroyCookie(undefined, 'nextauth.refreshToken')
+
+  authChannel.postMessage('signOut')
 
   Router.push('/')
 }
@@ -39,6 +44,17 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel('channel')
+
+    authChannel.onmessage = (message) => {
+      if (message.data === 'signOut') {
+        signOut()
+        return
+      }
+    }
+  }, [])
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
@@ -89,7 +105,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, signOut, user }}>
       {children}
     </AuthContext.Provider>
   )
--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut } = useContext(AuthContext)
 
   const useCanSeeMetrics = useCan({ permissions: ['metrics.list'] })
 
@@ -24,6 +24,8 @@ export default function Dashboard() {
       <h1>
         Dashboard {user?.email}
       </h1>
+
+      <button type="button" onClick={signOut}> Sign out </button>
       <Can permissions={['metrics.list']} >
         <div> Métricas </div>
       </Can>
