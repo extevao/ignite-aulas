57018ea1d0155e8fc37e35e8c2bd71837cbee0ec
Estevao <estevaovasques@hotmail.com>
2021-08-29 11:27:47 -0300

    hook de permissao

--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { useContext, useEffect } from "react"
+
 import { AuthContext } from "../context/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -7,15 +9,22 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const useCanSeeMetrics = useCan({ permissions: ['metrics.list'] })
+
+  console.log({ useCanSeeMetrics })
+
   useEffect(() => {
     api.get('/me')
       .then(response => console.log('[useEffect]', response.data))
   })
 
   return (
-    <h1>
-      Dashboard {user?.email}
-    </h1>
+    <>
+      <h1>
+        Dashboard {user?.email}
+      </h1>
+      {useCanSeeMetrics && <div> Métricas </div>}
+    </>
   )
 }
 
